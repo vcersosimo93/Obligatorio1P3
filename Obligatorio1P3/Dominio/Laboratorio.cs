a6963c96aaf812b3461612e570554725d4b1e33b
vcersosimo93 <65621966+vcersosimo93@users.noreply.github.com>
2021-04-10 22:03:49 -0300

    Add files via upload

--- Obligatorio1P3/Dominio/Laboratorio.cs
+++ Obligatorio1P3/Dominio/Laboratorio.cs
@@ -3,11 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Dominio
 {
     class Laboratorio
     {
+        private int id;
+        private string nombre;
+        private Pais paisOrigen;
+        private bool experiencia;
+
+
+        public int Id { get { return id; } }
+
+        public string Nombre
+        {
+            get { return nombre; }
+        }
+
+        public Pais PaisOrigen
+        {
+            get { return paisOrigen; }
+        }
+
+        public bool Experiencia
+        {
+            get { return experiencia; }
+        }
+
+
+        public Laboratorio(int id, string nombre, Pais paisOrigen, bool experiencia)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.paisOrigen = paisOrigen;
+            this.experiencia = experiencia;
+        }
+
 
     }
 }
