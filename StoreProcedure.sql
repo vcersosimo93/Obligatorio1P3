bdcf123231f43b2a3caacfe258ed298fa77bc097
vcersosimo93 <65621966+vcersosimo93@users.noreply.github.com>
2021-04-18 12:23:56 -0300

    Add files via upload

---
+++ StoreProcedure.sql
@@ -0,0 +1,77 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE RegistroUsuario
+
+	@docu int,
+	@contra varchar(50),
+	@rol varchar(50)
+
+AS
+	insert into usuario values (@docu, @contra, @rol);
+GO
+
+CREATE PROCEDURE LoginUsuario
+
+	@docu int,
+	@contra varchar(50),
+	@rol varchar(50)
+
+
+AS
+	begin 
+		select * from usuario order by documento
+	end
+
+GO
+
+
+
+CREATE PROCEDURE RegistroVacuna
+
+	@nombre varchar(50),
+	@laboratorios int,
+	@tipoVacuna varchar(50),
+	@cantDosis int, 
+	@lapsoDias int, 
+	@rangoEdad int, 
+	@efiCovid int, 
+	@efiHosp int,
+	@efiCTI int, 
+	@temperatura int,
+	@faseAprobacion int, 
+	@estatus int,
+	@aprobEme varchar(2),
+	@efectosAdversos varchar (50),
+	@precio int,
+	@produccion int,
+	@covax varchar(2),
+	@fechaEdicion date,
+	@ciEdicion int
+
+
+AS
+	insert into vacuna values (@nombre, @laboratorios, @tipoVacuna,@cantDosis ,@lapsoDias ,@rangoEdad ,@efiCovid ,@efiHosp, 
+	@efiCTI, @temperatura, @faseAprobacion, @estatus, @aprobEme, @efectosAdversos, @precio, @produccion, @covax,
+	@fechaEdicion, @ciEdicion);
+GO
+
+
+
